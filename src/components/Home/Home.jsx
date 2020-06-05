44001dbaddad9a2d7b31bb149d64a115b3a6d4da
Jose <j.a.ojeda@live.com>
2020-06-05 13:46:47 -0700

    add HELLO emitter

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,10 +3,15 @@ import { useSocket } from 'react-socket-io-hooks';
 import { useHistory } from 'react-router-dom';
 
 const Home = () => {
+
+
   const socket = useSocket();
   const history = useHistory();
-
+  
+  
   const handleClick = () => {
+    //use socket.emit to send events, the string name is how it ties to listener
+    socket.emit('HELLO');
     socket.emit('JOIN');
     history.push('/game');
   };
--- src/reducer.js
+++ src/reducer.js
@@ -1,18 +1,20 @@
 export const initialState = {
-    role: null,
-    started: false,
-    winner: null
+  role: null,
+  started: false,
+  winner: null
 };
 
 export default function reducer(state, action) {
-    switch(action.type) {
-        case 'JOIN':
-            return { ...state, role: action.payload, started: false, winner: null }
-        case 'START': 
-            return { ...state, started: true }
-        case 'RESULTS':
-            return { ...state, winner: action.payload }
-        default: 
-            return state;
-    }
+  switch(action.type) {
+    case 'JOIN':
+      return { ...state, role: action.payload, started: false, winner: null };
+    case 'START': 
+      return { ...state, started: true };
+    case 'RESULTS':
+      return { ...state, winner: action.payload };
+    case 'HELLO':
+      return { state };
+    default: 
+      return state;
+  }
 }
