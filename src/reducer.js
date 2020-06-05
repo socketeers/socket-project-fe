65997dbdfc354ae7491c6b8a836512081b4ccc07
Jaime Sanders <jaimelyn517@gmail.com>
2020-06-05 15:00:08 -0700

    allows players to drop and rejoin

--- src/reducer.js
+++ src/reducer.js
@@ -9,15 +9,15 @@ export default function reducer(state, action) {
   switch(action.type) {
     case 'JOIN':
       return { ...state, role: action.payload, started: false, winner: null };
-    case 'START': 
-      return { ...state, started: true };
+    case 'START':
+      return { ...state, started: action.payload };
     case 'RESULTS':
       return { ...state, winner: action.payload };
     case 'DRAW':
       return { ...state, tie: action.payload };
     case 'HELLO':
       return { state };
-    default: 
+    default:
       return state;
   }
 }
